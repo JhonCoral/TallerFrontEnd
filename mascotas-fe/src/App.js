eae775db316ea4e5be18e4e726f1b6e535123c91
JhonCoral <151975198+JhonCoral@users.noreply.github.com>
2024-01-11 00:14:49 -0500

    commit

--- mascotas-fe/src/App.js
+++ mascotas-fe/src/App.js
@@ -93,7 +93,7 @@ function App() {
         <div className="modal-dialog">
             <div className="modal-content">
             <div className="modal-header">
-                <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <h1 className="modal-title fs-5" id="exampleModalLabel">Solicitud de Adopcion</h1>
                 <button onClick={(e)=>limpiarCampos()} type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
               <div className="modal-body">
